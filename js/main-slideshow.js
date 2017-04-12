b0ff696e8a7da6d66ec2a97d28a8f68dc9a12a7d
Artiom <sinigur2006@gmail.com>
2017-04-13 01:46:13 +0300

    Upload brandly

---
+++ js/main-slideshow.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.mainSlideShow').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true
+    });
+});
